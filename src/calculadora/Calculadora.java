88637fc0b8d58b34629e3ae81d74bb53eb6ab61c
Rodrigo Arroyo <rodrigo2arroyo@gmail.com>
2026-06-25 08:25:57 -0500

    - Se agrego otro numero

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -5,6 +5,9 @@ public class Calculadora {
     public static void main(String[] args) {
         int numero = 5;
         System.out.println(numero);
+        
+        int numero2 = 8;
+        System.out.println(numero+numero2);
     }
     
 }
